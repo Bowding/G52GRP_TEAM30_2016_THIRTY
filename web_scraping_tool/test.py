5378cd936bea69e8b2917265be3b0d2d8da825f0
psynw1 <psynw1@exmail.nottingham.ac.uk>
2016-12-02 15:34:49 +0000

    Upload a web scraping tool, can ignore foreign characters

---
+++ web_scraping_tool/test.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://scholar.google.co.uk/citations?user=G0yAJAwAAAAJ&hl=en&oi=ao&cstart=0&pagesize=200"
+
+r = requests.get(url)
+
+soup = BeautifulSoup(r.content)
+
+name_data = soup.find_all("div", {"id": "gsc_prf_in"})[0]
+hIndex = soup.find_all("td", {"class": "gsc_rsb_std"})
+article = soup.find_all("tr", {"class": "gsc_a_tr"})
+
+print("Author Name: " + name_data.text)
+print("h-index (all time): " + hIndex[2].text)
+print("h-index (Since 2011): " + hIndex[3].text + "\n")
+
+x = 1
+
+for item in article:
+    print("Paper %d: "%(x) + item.find_all("a", {"class": "gsc_a_at"})[0].text.encode('ascii', 'ignore').decode('ascii'))
+    print("Year: " + item.find_all("td", {"class": "gsc_a_y"})[0].text.encode('ascii', 'ignore').decode('ascii'))
+    if item.find_all("td", {"class": "gsc_a_c"})[0].text != " ":
+        print("Cited by: " + item.find_all("td", {"class": "gsc_a_c"})[0].text.encode('ascii', 'ignore').decode('ascii'))
+    else:
+        print("Cited by: 0\n")
+    print("Co-Authors: " + item.find_all("div", {"class" : "gs_gray"})[0].text.encode('ascii', 'ignore').decode('ascii') + "\n")
+    x += 1
