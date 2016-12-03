874ffacdb28900b5ce6995c5d5caf7cab33ecf25
AkbarCS <psyam12@nottingham.ac.uk>
2016-12-03 22:56:37 +0000

    functionality added to retrieve URLs of co-authors and to access them for data scraping

--- web_scraping_tool/google_scholar_scraping.py
+++ web_scraping_tool/google_scholar_scraping.py
@@ -1,28 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
+	
+def basicInformation(url):
+	r = requests.get(url)
 
-url = "https://scholar.google.co.uk/citations?user=G0yAJAwAAAAJ&hl=en&oi=ao&cstart=0&pagesize=200"
+	soup = BeautifulSoup(r.content, "html.parser")
 
-r = requests.get(url)
+	name_data = soup.find_all("div", {"id": "gsc_prf_in"})[0]
+	hIndex = soup.find_all("td", {"class": "gsc_rsb_std"})
+	article = soup.find_all("tr", {"class": "gsc_a_tr"})
 
-soup = BeautifulSoup(r.content)
+	print("Author Name: " + name_data.text)
+	print("h-index (all time): " + hIndex[2].text)
+	print("h-index (Since 2011): " + hIndex[3].text + "\n")
 
-name_data = soup.find_all("div", {"id": "gsc_prf_in"})[0]
-hIndex = soup.find_all("td", {"class": "gsc_rsb_std"})
-article = soup.find_all("tr", {"class": "gsc_a_tr"})
+	x = 1
 
-print("Author Name: " + name_data.text)
-print("h-index (all time): " + hIndex[2].text)
-print("h-index (Since 2011): " + hIndex[3].text + "\n")
-
-x = 1
-
-for item in article:
-    print("Paper %d: "%(x) + item.find_all("a", {"class": "gsc_a_at"})[0].text.encode('ascii', 'ignore').decode('ascii'))
-    print("Year: " + item.find_all("td", {"class": "gsc_a_y"})[0].text.encode('ascii', 'ignore').decode('ascii'))
-    if item.find_all("td", {"class": "gsc_a_c"})[0].text != " ":
-        print("Cited by: " + item.find_all("td", {"class": "gsc_a_c"})[0].text.encode('ascii', 'ignore').decode('ascii'))
-    else:
-        print("Cited by: 0\n")
-    print("Co-Authors: " + item.find_all("div", {"class" : "gs_gray"})[0].text.encode('ascii', 'ignore').decode('ascii') + "\n")
-    x += 1
+	for item in article:
+		print("Paper %d: "%(x) + item.find_all("a", {"class": "gsc_a_at"})[0].text.encode('ascii', 'ignore').decode('ascii'))
+		print("Year: " + item.find_all("td", {"class": "gsc_a_y"})[0].text.encode('ascii', 'ignore').decode('ascii'))
+		if item.find_all("td", {"class": "gsc_a_c"})[0].text != " ":
+			print("Cited by: " + item.find_all("td", {"class": "gsc_a_c"})[0].text.encode('ascii', 'ignore').decode('ascii'))
+		else:
+			print("Cited by: 0\n")
+		print("Co-Authors: " + item.find_all("div", {"class" : "gs_gray"})[0].text.encode('ascii', 'ignore').decode('ascii') + "\n")
+		x += 1	
+		
+	for link in soup.find_all("a", {"class": "gsc_rsb_aa"}):
+		link = "https://scholar.google.co.uk" + link.get('href')
+		print(link)
+		#basicInformation(link) 
+		
+if __name__ == "__main__":
+	url = "https://scholar.google.co.uk/citations?user=33ftCdEAAAAJ&hl=en"
+	basicInformation(url)
