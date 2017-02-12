6bb016ee04a100948f8a0da4df8e6028f119c8ce
AkbarCS <psyam12@nottingham.ac.uk>
2017-02-12 01:53:17 +0000

    Added functionality to connect python web scraping tool to sql database in the file web_scraping_tool_pymysql. This extracts information from a scholar's profile (i.e. papers published) and adds it to the sql database.

--- web_scraping_tool/web_scraping_tool_pymysql.py
+++ web_scraping_tool/web_scraping_tool_pymysql.py
@@ -6,7 +6,7 @@ url = "https://scholar.google.co.uk/citations?user=G0yAJAwAAAAJ&hl=en&oi=ao&csta
 
 r = requests.get(url)
 
-soup = BeautifulSoup(r.content)
+soup = BeautifulSoup(r.content, "html.parser")
 
 name_data = soup.find_all("div", {"id": "gsc_prf_in"})[0]
 hIndex = soup.find_all("td", {"class": "gsc_rsb_std"})
@@ -16,44 +16,61 @@ print("Author Name: " + name_data.text)
 print("h-index (all time): " + hIndex[2].text)
 print("h-index (Since 2011): " + hIndex[3].text + "\n")
 
-x = 1
-
-for item in article:
-    print("Paper %d: "%(x) + item.find_all("a", {"class": "gsc_a_at"})[0].text)
-    print("Year: " + item.find_all("td", {"class": "gsc_a_y"})[0].text)
-    if item.find_all("td", {"class": "gsc_a_c"})[0].text != " ":
-        print("Cited by: " + item.find_all("td", {"class": "gsc_a_c"})[0].text + "\n")
-    else:
-        print("Cited by: 0\n")
-    x += 1
-
-
-
-name = name_data.text
-h = hIndex[2].text
-
 try:
-    print("Connecting to mySQL.....")
-    conn = pymysql.connect(user="root", passwd="", host="127.0.0.1", port=3306, database="googlescholar")
-    print("Connection established!")
+	print("Connecting to mySQL.....")
+	conn = pymysql.connect(host='localhost', db='googlescholardb', user='root', password='', cursorclass=pymysql.cursors.DictCursor)
+	print("Connection established!")
 except:
-    print("Connection Failed!")
+	print("Connection Failed!")
 
 cur = conn.cursor()
 
-print("INSERT INTO profile (aName, NumPaper, hIndex) VALUES ('%s', %d, %d)" %(name, int(x), int(h)))
+x = 1
+for item in article:
+	paperName = item.find_all("a", {"class": "gsc_a_at"})[0].text.encode('ascii', 'ignore').decode('ascii')
+	print("Paper: " + str(x) + " " + paperName)
+	year = item.find_all("td", {"class": "gsc_a_y"})[0].text.encode('ascii', 'ignore').decode('ascii')
+	print("Year: " + year)
+	
+	citationNumber = item.find_all("td", {"class": "gsc_a_c"})[0].text.encode('ascii', 'ignore').decode('ascii')
+	seq_type = type(citationNumber)
+	citationNumber = seq_type().join(filter(seq_type.isdigit, citationNumber))
+	
+	if citationNumber != "":
+		print("Cited by: " + citationNumber)
+		print("INSERT into papers (paperName, author, yearPublished, numberOfCitations) VALUES ('%s','%s', %d, %d)\n" % (paperName, name_data.text, int(year), int(citationNumber)))
+		try:
+			cur.execute("INSERT into papers (paperName, author, yearPublished, numberOfCitations) VALUES ('%s','%s', %d, %d)" % (paperName, name_data.text, int(year), int(citationNumber)))
+			conn.commit()
+		except ValueError:
+			print("Failed inserting....")	
+	else:
+		print("Cited by: 0")
+		print("INSERT into papers (paperName, author, yearPublished) VALUES ('%s','%s', %d)\n" % (paperName, name_data.text, int(year)))
+		try:
+			cur.execute("INSERT into papers (paperName, author, yearPublished) VALUES ('%s','%s', %d)" % (paperName, name_data.text, int(year)))
+			conn.commit()
+		except ValueError:
+			print("Failed inserting....")	
+			
+	x += 1
 
+print("\nINSERT INTO profile (aName, NumPaper, hIndex) VALUES ('%s', %d, %d)\n" % (name_data.text, int(x), int(hIndex[2].text)))
 try:
-    cur.execute("INSERT INTO profile (aName, NumPaper, hIndex) VALUES ('%s', %d, %d)" %(name, int(x), int(h)))
-    conn.commit()
+	cur.execute("INSERT INTO profile (aName, NumPaper, hIndex) VALUES ('%s', %d, %d)" % (name_data.text, int(x), int(hIndex[2].text)))
+	conn.commit()
 except:
-    print("Failed inserting....")
-    
+	print("Failed inserting....")
+	
 cur.execute("SELECT * FROM `profile`")
-
-
 for row in cur:
-    print(row)
+	print(row)
+
+print("\n")
+	
+cur.execute("SELECT * FROM `papers`")
+for row in cur:
+	print(row)
 
 cur.close()
 conn.close()
