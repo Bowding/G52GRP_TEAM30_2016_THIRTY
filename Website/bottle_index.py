ff173febec9ef7afe691ffad1f1133e852d5031d
Bowding <bowding@outlook.com>
2017-02-25 19:45:51 +0000

    fix paper number problem and escape string when insert
    and link png files to bottle

--- Website/bottle_index.py
+++ Website/bottle_index.py
@@ -3,30 +3,40 @@ import bottle_pymysql
 import requests
 from bottle import template, static_file
 from bs4 import BeautifulSoup
+#connect to db
+def connect_to_db():
+    try:
+        print("Connecting to mySQL.....")
+        plugin = bottle_pymysql.Plugin(dbuser = 'root', dbpass = 'CHEERs0251', dbname = 'googlescholardb')
+        #conn = pymysql.connect(host='localhost', db='googlescholardb', user='root', password='', cursorclass=pymysql.cursors.DictCursor)
+        bottle.install(plugin)
+        print("Connection established!")
+    except:
+        print("Connection Failed!")
 
 
+
+#display website
 @bottle.route('/')
 def login():
     return template("index.html")
 
+#link static files
 @bottle.route('/<filename>.css')
 def stylesheets(filename):
     return static_file('{}.css'.format(filename), root='./')
 
-try:
-    print("Connecting to mySQL.....")
-    plugin = bottle_pymysql.Plugin(dbuser = 'root', dbpass = 'CHEERs0251', dbname = 'googlescholardb')
-    #conn = pymysql.connect(host='localhost', db='googlescholardb', user='root', password='', cursorclass=pymysql.cursors.DictCursor)
-    bottle.install(plugin)
-    print("Connection established!")
-except:
-    print("Connection Failed!")
+@bottle.route('/<filename>.png')
+def stylesheets(filename):
+    return static_file('{}.png'.format(filename), root='./')
 
-    #cur = conn.cursor()
+@bottle.route('/<filename>.ico')
+def stylesheets(filename):
+    return static_file('{}.ico'.format(filename), root='./')
 
+connect_to_db()
 
-
-
+#handle user input
 @bottle.route('/test', method="POST")
 def formhandler(pymydb):
     """Handle the form submission"""
@@ -50,63 +60,89 @@ def formhandler(pymydb):
 
     target_url_area = match_soup.find("h3", {"class": "gsc_1usr_name"})
     target_url = "https://scholar.google.co.uk" + target_url_area.find("a").get("href")
-    #access to target author page
-    r = requests.get(target_url)
+
+    url = target_url.replace("oe=ASCII","oi=ao&cstart=0&pagesize=100")
+
+    #access to target author page - first page
+    r = requests.get(url)
     soup = BeautifulSoup(r.content, "html.parser")
 
+    #get profile
     name_data = soup.find_all("div", {"id": "gsc_prf_in"})[0]
     hIndex = soup.find_all("td", {"class": "gsc_rsb_std"})
-    article = soup.find_all("tr", {"class": "gsc_a_tr"})
 
     print("Author Name: " + name_data.text)
     print("h-index (all time): " + hIndex[2].text)
     print("h-index (Since 2011): " + hIndex[3].text + "\n")
 
     x = 1
-    for item in article:
-        paperName = item.find_all("a", {"class": "gsc_a_at"})[0].text.encode('ascii', 'ignore').decode('ascii')
-        print("Paper: " + str(x) + " " + paperName)
-        year = item.find_all("td", {"class": "gsc_a_y"})[0].text.encode('ascii', 'ignore').decode('ascii')
-        print("Year: " + year)
+    cstart = 0
+    while(1):
+        article = soup.find_all("tr", {"class": "gsc_a_tr"})
+        for item in article:
+            paperName = item.find_all("a", {"class": "gsc_a_at"})[0].text.encode('ascii', 'ignore').decode('ascii')
+            print("Paper: " + str(x) + " " + paperName)
+            year = item.find_all("td", {"class": "gsc_a_y"})[0].text.encode('ascii', 'ignore').decode('ascii')
+            print("Year: " + year)
 
-        citationNumber = item.find_all("td", {"class": "gsc_a_c"})[0].text.encode('ascii', 'ignore').decode('ascii')
-        seq_type = type(citationNumber)
-        citationNumber = seq_type().join(filter(seq_type.isdigit, citationNumber))
+            citationNumber = item.find_all("td", {"class": "gsc_a_c"})[0].text.encode('ascii', 'ignore').decode('ascii')
+            seq_type = type(citationNumber)
+            citationNumber = seq_type().join(filter(seq_type.isdigit, citationNumber))
+            
+            if citationNumber != "" and year != "":
+                print("Cited by: " + citationNumber)
+                print("INSERT into papers (paperName, author, yearPublished, numberOfCitations) VALUES ('%s','%s', %d, %d)\n" % (paperName, name_data.text, int(year), int(citationNumber)))
+                try:
+                    pymydb.execute("INSERT into papers (paperName, author, yearPublished, numberOfCitations) VALUES ('%s','%s', %d, %d)" % (paperName.replace("'","\\\'"), name_data.text, int(year), int(citationNumber)))
+                    #conn.commit()
+                except ValueError:
+                    print("Failed inserting....")   
+            elif citationNumber == "" and year != "":
+                print("Cited by: 0")
+                print("INSERT into papers (paperName, author, yearPublished) VALUES ('%s','%s', %d)\n" % (paperName, name_data.text, int(year)))
+                try:
+                    pymydb.execute("INSERT into papers (paperName, author, yearPublished) VALUES ('%s','%s', %d)" % (paperName.replace("'","\\\'"), name_data.text, int(year)))
+                    #conn.commit()
+                except ValueError:
+                    print("Failed inserting....")
+            elif year == "" and citationNumber != "":
+                print("No publish year")
+                print("INSERT into papers (paperName, author, numberOfCitations) VALUES ('%s','%s', %d)\n" % (paperName, name_data.text, int(citationNumber)))
+                try:
+                    pymydb.execute("INSERT into papers (paperName, author, numberOfCitations) VALUES ('%s','%s', %d)" % (paperName.replace("'","\\\'"), name_data.text, int(citationNumber)))
+                    #conn.commit()
+                except ValueError:
+                    print("Failed inserting....")
+            else:
+                print("No publish year and citation number")
+                print("INSERT into papers (paperName, author) VALUES ('%s','%s')\n" % (paperName, name_data.text))
+                try:
+                    pymydb.execute("INSERT into papers (paperName, author) VALUES ('%s','%s')" % (paperName.replace("'","\\\'"), name_data.text))
+                    #conn.commit()
+                except ValueError:
+                    print("Failed inserting....")
+                    
+            x += 1
+
+        #get this page paper number
+        this_page_NumPaper_range = soup.find("span", {"id": "gsc_a_nn"}).text
+
+        print("======="+this_page_NumPaper_range.split("–")[0])
+
+        this_page_NumPaper = int(this_page_NumPaper_range.split("–")[1])
+        cstart +=100
+
+        if(this_page_NumPaper < cstart):
+            x -= 1
+            break
         
-        if citationNumber != "" and year != "":
-            print("Cited by: " + citationNumber)
-            print("INSERT into papers (paperName, author, yearPublished, numberOfCitations) VALUES ('%s','%s', %d, %d)\n" % (paperName, name_data.text, int(year), int(citationNumber)))
-            try:
-                pymydb.execute("INSERT into papers (paperName, author, yearPublished, numberOfCitations) VALUES ('%s','%s', %d, %d)" % (paperName, name_data.text, int(year), int(citationNumber)))
-                #conn.commit()
-            except ValueError:
-                print("Failed inserting....")   
-        elif citationNumber == "":
-            print("Cited by: 0")
-            print("INSERT into papers (paperName, author, yearPublished) VALUES ('%s','%s', %d)\n" % (paperName, name_data.text, int(year)))
-            try:
-                pymydb.execute("INSERT into papers (paperName, author, yearPublished) VALUES ('%s','%s', %d)" % (paperName, name_data.text, int(year)))
-                #conn.commit()
-            except ValueError:
-                print("Failed inserting....")
-        elif year == "":
-            print("No publish year")
-            print("INSERT into papers (paperName, author, numberOfCitations) VALUES ('%s','%s', %d)\n" % (paperName, name_data.text, int(citationNumber)))
-            try:
-                pymydb.execute("INSERT into papers (paperName, author, numberOfCitations) VALUES ('%s','%s', %d)" % (paperName, name_data.text, int(citationNumber)))
-                #conn.commit()
-            except ValueError:
-                print("Failed inserting....")
-        else:
-            print("No publish year and citation number")
-            print("INSERT into papers (paperName, author) VALUES ('%s','%s')\n" % (paperName, name_data.text))
-            try:
-                pymydb.execute("INSERT into papers (paperName, author) VALUES ('%s','%s')" % (paperName, name_data.text))
-                #conn.commit()
-            except ValueError:
-                print("Failed inserting....")
-                
-        x += 1
+        #get next page url
+        url = target_url.replace("oe=ASCII","oi=ao&cstart=%d&pagesize=100" % (cstart))
+        #access to next page
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, "html.parser")
+        
+
 
     print("\nINSERT INTO profile (aName, NumPaper, hIndex) VALUES ('%s', %d, %d)\n" % (name_data.text, int(x), int(hIndex[2].text)))
     try:
